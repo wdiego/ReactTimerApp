e31f2a09b855faeccd06c46fbbbda6060ad40242
Wanderson Diego <wdiegomt@gmail.com>
2016-12-19 11:59:03 -0200

    Add navigation

---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Timer = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1 className="text-center">Timer</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = Timer;
